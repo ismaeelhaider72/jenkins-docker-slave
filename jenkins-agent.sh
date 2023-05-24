e18bd82737d33fb6cbb20822ad01e8287d7cef85
ismaeelhaider72 <66254080+ismaeelhaider72@users.noreply.github.com>
2023-05-24 23:45:05 +0500

    Create jenkins-agent.sh

---
+++ jenkins-agent.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Jenkins URL and secret
+JENKINS_URL="http://192.168.18.191:8080"
+AGENT_SECRET="ea89e2312a656a820d6dfcf2ea98e5fc995bdcede09b29c618d2a339b442d862"
+
+# Jenkins JNLP agent parameters
+PARAMS="-url $JENKINS_URL -workDir /home/jenkins/agent -secret $AGENT_SECRET"
+
+# Start the Jenkins JNLP agent
+java $JAVA_OPTS -cp /usr/share/jenkins/agent/agent.jar hudson.remoting.jnlp.Main -headless $PARAMS
