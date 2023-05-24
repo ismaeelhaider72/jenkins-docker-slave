5858aa425658e652f2e8e62ee44f5298d2a52e5e
ismaeelhaider72 <66254080+ismaeelhaider72@users.noreply.github.com>
2023-05-25 02:13:16 +0500

    Update jenkins-agent.sh

--- jenkins-agent.sh
+++ jenkins-agent.sh
@@ -6,7 +6,7 @@ AGENT_SECRET="ea89e2312a656a820d6dfcf2ea98e5fc995bdcede09b29c618d2a339b442d862"
 
 # Jenkins JNLP agent parameters
 # PARAMS="-jnlpUrl $JENKINS_URL/docker-slave.jnlp -workDir /home/jenkins/agent"
-PARAMS="-jnlpUrl $JENKINS_URL/computer/docker-slave/slave-agent.jnlp -workDir /home/jenkins/agent_workspace"
+PARAMS="-jnlpUrl $JENKINS_URL/manage/computer/docker-slave/slave-agent.jnlp -workDir /home/jenkins/agent_workspace"
 
 # Start the Jenkins JNLP agent
 java $JAVA_OPTS -jar /usr/share/jenkins/agent/agent.jar $PARAMS -secret $AGENT_SECRET
